7690949182b56319ce61461365fd7895daf7c9e5
Julian Lopez <julian12lopez@gmail.com>
2020-04-14 12:32:22 -0400

    built timer logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,18 @@
-const quiz =document.getElementById("quiz");
+const quiz = document.getElementById('quiz');
+const quizTimer = document.getElementById('quiz--timer');
+const quizButton = document.getElementById('quiz--start');
+
+let timeLeft = 60;
 
 const startTimer = () => {
-
-    //setInterval(() => .1000 )
-}
-
-
+setInterval(() => {
+    timeLeft--;
+    if(timeLeft <= 0) return;
+    quizTimer.innerText = timeLeft;
+} ,1000 );
+};
 
 const startQuiz = () => {
-
-}
+    startTimer();
+};
+quizButton.addEventListener('click', startQuiz);
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,17 +1,17 @@
 const question = [
     {
-        q: "What is a rock?",
-        c: "0",
-        a: ["A rock", "A tree", "A cat", "A house"]
+        q: 'What is a rock?',
+        c: '0',
+        a: ['A rock", "A tree", "A cat", "A house']
     },
     {
-        q: "What is a tree?",
-        c: "3",
-        a: ["A rock", "A tree", "A cat", "A house"]
+        q: 'What is a tree?',
+        c: '3',
+        a: ['A rock", "A tree", "A cat", "A house']
     },
     {
-        q: "What is a house?",
-        c: "1",
-        a: ["A rock", "A tree", "A cat", "A house"]
+        q: 'What is a house?',
+        c: '1',
+        a: ['A rock", "A tree", "A cat", "A house']
     }
 ]
