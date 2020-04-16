9e7aafc790b305c0c970007e7440df09cfd89b6a
Julian Lopez <julian12lopez@gmail.com>
2020-04-16 14:05:45 -0400

    goes through 3 questions but added 5, needs score and endGame()

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,20 +6,51 @@ let timeLeft = 60;
 let currQuestions = 0;
 
 const startTimer = () => {
+    //run this block of code every 1000ms
+    //
 setInterval(() => {
     timeLeft--;
-    if(timeLeft <= 0) return;
     quizTimer.innerText = timeLeft;
 } ,1000 );
 };
 
+const checkAnswer = (userAnswer, correctAnswer) => {
+    console.log(userAnswer);
+    console.log(correctAnswer);
+
+    if(currQuestions == questions.length || timeLeft == 0){
+        console.log("Game Over");
+        console.
+        endQuiz();
+    }
+    console.log(currQuestions);
+    console.log(questions.length);
+
+    if(userAnswer !== correctAnswer ){
+        timeLeft -= 10;
+        currQuestions++;
+        renderQuestion(questions[currQuestions]);
+
+        
+    }else{
+    currQuestions++;
+    console.log(currQuestions);
+    renderQuestion(questions[currQuestions]);
+    
+    }
+
+}
+
 const renderQuestion = ({q, a, c}) => {
+   //index = Math.floor(Math.random() * questions.length);
+
+
     quiz.innerHTML = '';
 
     const div = document.createElement('div');
-    div.className = 'flex column align-center w-100'
-    const buttonContainer = document.createElement('div')
-    buttonContainer.className = 'flex row wrap align-center justify-center w-100'
+    div.className = 'flex column align-center w-100';
+    const buttonContainer = document.createElement('div');
+    buttonContainer.className = 'flex row wrap align-center justify-center w-100';
     const h3 = document.createElement('h3');
     h3.innerText = q;
     div.append(h3);
@@ -27,8 +58,9 @@ const renderQuestion = ({q, a, c}) => {
         const button = document.createElement('button');
         button.innerText = answer;
         //create correct answer functionality
-        button.addEventListener('click',() => null );
+        button.addEventListener('click',() => checkAnswer(index, c) );
         buttonContainer.append(button);
+        
     });
 
     div.append(buttonContainer);
@@ -37,6 +69,12 @@ const renderQuestion = ({q, a, c}) => {
 
 const startQuiz = () => {
     startTimer();
-    renderQuestion(questions[0]);
+    
+
+    renderQuestion(questions[currQuestions]);
+    console.log("hello");
 };
+
 quizButton.addEventListener('click', startQuiz);
+
+//when adding code you want it to incorporate in without breaking your other code
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,17 +1,27 @@
 const questions = [
     {
         q: 'What is a rock?',
-        c: '0',
+        c: 0,
         a: ['A rock', 'A tree', 'A cat', 'A house']
     },
     {
         q: 'What is a tree?',
-        c: '3',
+        c: 1,
         a: ['A rock', 'A tree', 'A cat', 'A house']
     },
     {
         q: 'What is a house?',
-        c: '1',
+        c: 3,
+        a: ['A rock', 'A tree', 'A cat', 'A house']
+    },
+    {
+        q: 'What is a house?',
+        c: 3,
+        a: ['A rock', 'A tree', 'A cat', 'A house']
+    },
+    {
+        q: 'What is a house?',
+        c: 3,
         a: ['A rock', 'A tree', 'A cat', 'A house']
     }
 ]
