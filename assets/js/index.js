25ff8eb798c2a61e859989341864baf215fc2776
Julian Lopez <julian12lopez@gmail.com>
2020-04-20 20:50:53 -0400

    local storate contains nickname and score

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,13 +2,14 @@ const quiz = document.getElementById('quiz');
 const quizTimer = document.getElementById('quiz--timer');
 const quizButton = document.getElementById('quiz--start');
 
+let interval
 let timeLeft = 60;
 let currQuestions = 0;
 
 const startTimer = () => {
     //run this block of code every 1000ms
     //
-setInterval(() => {
+interval = setInterval(() => {
     timeLeft--;
     quizTimer.innerText = timeLeft;
 } ,1000 );
@@ -20,7 +21,6 @@ const checkAnswer = (userAnswer, correctAnswer) => {
 
     if(currQuestions == questions.length || timeLeft == 0){
         console.log("Game Over");
-        console.
         endQuiz();
     }
     console.log(currQuestions);
@@ -44,8 +44,6 @@ const checkAnswer = (userAnswer, correctAnswer) => {
 
 const renderQuestion = ({q, a, c}) => {
    //index = Math.floor(Math.random() * questions.length);
-
-
     quiz.innerHTML = '';
 
     const div = document.createElement('div');
@@ -67,31 +65,47 @@ const renderQuestion = ({q, a, c}) => {
     div.append(buttonContainer);
     quiz.append(div);
 }
+
 const endQuiz = () => {
     quiz.innerHTML = '';
+    clearInterval(interval);
     const div = document.createElement('div');
     const input = document.createElement('input');
     input.id = 'quiz--highscore';
     input.placeholder = 'Enter you Nick Name';
     const button = document.createElement('button');
+    button.innerText = 'Add Your Score!'
     button.addEventListener('click', () => {
         const input = document.getElementById('quiz--highscore');
+        const score = quizTimer.innerText;
         const nickName = input.value;
-        localStorage.setItem('user',nickName);
+        const userJSON = localStorage.getItem(nickName);
+    if(userJSON){
+        const user = JSON.parse(userJSON)
+        const newUser = [...user, score];
+       return localStorage.setItem(nickName, JSON.stringify(newUser))
+    }
+        localStorage.setItem(nickName, JSON.stringify([score]));
     });
     div.append(input);
     div.append(button);
     quiz.append(div);
 }
 
-const handleQuiz = () => {
-    startTimer();
+
+
+const handleQuiz = (isStart) => {
+    if(isStart) startTimer();
+    if(currQuestions < questions.length || timeLeft <= 0 ){
+        renderQuestion(questions[currQuestions]);
+    }
+
     
 
-    renderQuestion(questions[currQuestions]);
-    //console.log("hello");
 };
 
-quizButton.addEventListener('click', handleQuiz);
+
+
+quizButton.addEventListener('click', () => handleQuiz(true));
 
 //when adding code you want it to incorporate in without breaking your other code
